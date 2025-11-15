70cd6b9f5f6424eca6342596f78ba26895037caa
Vladimir Romashchenko <eaglesemanation@gmail.com>
2025-11-15 04:32:45 -0500

    feat(term): Fish basic setup, ghostty keybinds

---
+++ dot-config/fish/config.fish
@@ -0,0 +1,26 @@
+#!/bin/fish
+
+# Replace default greeting with empty string
+set fish_greeting
+# Nice looking shell prompt
+starship init fish | source
+
+set -Ux PASSWORD_STORE_DIR ~/.local/share/pass-store
+
+# Use gpg-agent when present and there is a key with Authenticate cap
+if type -q gpg-agent; and type -q gpg; and gpg --list-keys --with-keygrip | grep "\[A\]" >/dev/null
+    set -e SSH_AGENT_PID
+    if [ "$gnupg_SSH_AUTH_SOCK_by" != "$fish_pid" ]
+        set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    end
+    set -gx GPG_TTY (tty)
+    gpg-connect-agent updatestartuptty /bye >/dev/null
+end
+
+# Auto install plugin manager
+if status is-interactive; and not test -e ~/.config/fish/functions/fisher.fish
+    curl --silent --location https://git.io/fisher | source
+    fisher install jorgebucaran/fisher
+    # Fix autocomplete for pass-otp
+    fisher install mserajnik/fish-completions-pass-extensions
+end
